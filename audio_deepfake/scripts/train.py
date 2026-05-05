e69e4bfb47ca1c19a6706e6fbc9fcb05c56f0e45
MeryemAlMoumi <meryemalmoumi1@gmail.com>
2026-05-05 21:44:07 +0100

    Update train.py

--- audio_deepfake/scripts/train.py
+++ audio_deepfake/scripts/train.py
@@ -1,7 +1,7 @@
 from training.cross_val import run_cross_validation
 
 CONFIG = {
-    'data_dir': '/kaggle/input/datasets/meryemalmoumi/dataset/data',
+    'data_dir': '/kaggle/input/datasets/meryemalmoumi/class_data/data',
     'n_folds': 5,
     'wav2vec2_name': 'facebook/wav2vec2-base',
     'embedding_dim': 256,
